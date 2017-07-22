16a0cae0aa0d4f530f14d680f881664262dbeabc
archerrrr <1020363151@qq.com>
2017-07-22 02:48:37 -0700

    start

---
+++ settings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*
+
+class Settings():
+    """描述游戏的各项设置参数。"""
+    
+    def __init__(self):
+        self.screen_width = 100
+        self.screen_height = 100
+        self.bg_color = (100, 255, 100)
+        
+    
